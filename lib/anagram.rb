a2bc521eb764567750147bd7dab29aaa960458bf
majaliju <majalijunyc@gmail.com>
2022-07-30 20:49:49 -0400

    commit tree 746b202e40ad04d66ebd211a80b416f7c7a3c0be

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match array
+    array.filter do |str|
+      word.chars.sort == str.chars.sort
+    end
+  end
+end
+
